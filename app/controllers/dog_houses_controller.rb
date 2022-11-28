4fdfd884945d7923ca3c196bedeb9d9866044b8e
Ronny <ronnybet41@gmail.com>
2022-11-28 15:53:58 +0300

    completed assignment

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -5,6 +5,16 @@ class DogHousesController < ApplicationController
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
   end
+  def reviews_index
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+    render json: reviews, include: :dog_house
+  end
+
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
+  end
 
   private
 
